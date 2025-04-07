303a63092c954e7161fd7ce017f115accf9165ef
Almsdel <amccalment2@gmail.com>
2025-04-07 17:09:33 -0600

    Add files via upload
    broke and fix stuff like. a good dev

--- game.py
+++ game.py
@@ -5,8 +5,14 @@ import gamefunctions
 name = input('Enter your name please: ')
 gamefunctions.playerStats['Name'] = name
 print(f'Welcome {name}.')
-playerInput = input(f'What will you do today?\n\n1) Go out and hunt a monster\n2) Stay the night at the inn (Heal): 5g\n3) Quit\n4) Stats\n5) Shop\n6) Inventory\n\nEnter a number choice here: ')
+input(f'You wake up in a village, sweaty and trembling as if you were chased out the night before.\nPress enter to continue')
+input(f'Looking to the nightstand, a club sits. You pick it up as you get off the bed, but as you do the club seemingly absorbs into you!')
+input('A surge of power channels through you, and then nothing...')
+input('"Maybe I should vist the shop and see what else i could absorb. That power felt amazing", you say to yourself')
+input('Just then the inn maid walks into your room, letting you know that your stay is over unless you can cover the coin needed. You exit the inn')
+print()
 
+playerInput = input(f'What will you do today?\n\n1) Go out and hunt a monster\n2) Stay the night at the inn (Heal): 5g\n3) Quit\n4) Stats\n5) Shop\n6) Inventory\n7) Save and Quit\n8) Load\n\nEnter a number choice here: ')
 
 while playerInput != '3':
    
@@ -15,28 +21,29 @@ while playerInput != '3':
     gamefunctions.attackMoment()
       
    elif playerInput == '2' and gamefunctions.playerStats['Gold'] >= 5:
-    gamefunctions.rest()
+        gamefunctions.rest()
    
    elif playerInput == '4':
       for i in gamefunctions.playerStats:
          print(i, gamefunctions.playerStats[i])
       input('press key to continue')
+
    elif playerInput == '5':
       gamefunctions.town_shop()
+
    elif playerInput == '6':
       gamefunctions.inv()
-    
+
+   elif playerInput == '7':
+      gamefunctions.savquit()
       
+
+   elif playerInput == '8':
+      gamefunctions.load()
+
       
       
    else:
         print('Please choose one of the options asshole')
         
-   playerInput = input(f'What will you do today?\n\n1) Go out and hunt a monster\n2) Stay the night at the inn (Heal): 5g\n3) Quit\n4) Stats\n5) Shop\n6) Inventory\n\nEnter a number choice here: ')
-          
-          
-      
-
-
-    
-      
+   playerInput = input(f'What will you do today?\n\n1) Go out and hunt a monster\n2) Stay the night at the inn (Heal): 5g\n3) Quit\n4) Stats\n5) Shop\n6) Inventory\n7) Save and Quit\n8) Load\n\nEnter a number choice here: ')
--- gamefunctions.py
+++ gamefunctions.py
@@ -2,9 +2,9 @@
 #This is a header for Game fuctions python 
 import random
 import math
+import json
 
 
-#part 1
 def purchase_item(itemPrice,startingMoney,quantityToPurchase= 1):
     purchase = startingMoney - (itemPrice * quantityToPurchase)
     if purchase > 0:
@@ -13,8 +13,12 @@ def purchase_item(itemPrice,startingMoney,quantityToPurchase= 1):
         quantityToPurchase = (startingMoney / itemPrice)
         purchase = startingMoney - (itemPrice * quantityToPurchase)
         return print(f'Insuficant funds. Instead you can purchase {int(quantityToPurchase)} with a remaining {purchase}')
+
 def new_random_monster():
-    chance = random.randint(1,6)
+    '''Picks one of three monsters for player to fight
+        all of which are goblins
+    '''
+    chance = random.randint(1,3)
     if chance ==1:
         goblin = {'name':'a goblin', 'description':'little guy haunched up on his heels', 'health' : 2, 'power' : 1, 'money' : 1.7}
     
@@ -24,12 +28,10 @@ def new_random_monster():
     
         return goblin_raider
     else:
-        goblin_archer = {'name':'a goblin archer', 'description':'little guy haunched up on his heels with a bow', 'health' : 1, 'power' : 5, 'money' : 3}
+        goblin_archer = {'name':'a goblin archer', 'description':'little guy haunched up on his heels with a bow', 'health' : 1, 'power' : 3, 'money' : 3}
     
         return goblin_archer
-
-
-#part 2   
+ 
 def print_welcome(name, width=0):
     '''
     Doctsting here: letting you know this fuction prints given name with "Hello," 
@@ -38,6 +40,7 @@ def print_welcome(name, width=0):
     title = "Hello, " + name +'!'
     print(f'{title:o^20}')
     #o was added for ease of sight
+
 def print_shop_menu(item1Name, item1Price, item2Name, item2Price):
     """Whoa this is a docstring"""
     item2Price = f'{item2Price:.2f}'
@@ -80,60 +83,92 @@ def test_function():
     purchase_item(12, 80)
 
 def attackMoment():
+    '''Player fights a monster from new_random_monster
+       all actions are dictated by 1 or 2 
+       attackInput is the variable that lets user pick an action to fight or run. 
+    '''
+    monster = new_random_monster()
+    monsterHP = monster.copy()
     print(f"You've encountered a monster! Its a {monster['name']}. Its a {monster['description']}.")
     attackInput = input(f'What will you do?\n\n1) Attack (Lose {monster['power']}hp)\n2) Run (Return to town)\n')
-    if attackInput =='1':
-      monsterHP = monster.copy()
-      while monsterHP['health'] > 0:
-        monsterHP['health'] -= playerStats['Attack']
-      if monsterHP['health'] <= 0:
-        input(f'You won! Gained {monsterHP['money']} Gold\nPress any key to continue.')
-        playerStats['Gold'] += monsterHP['money']
+    print(f'{playerStats['Name']}: {playerStats["Health"]} hp')
+    while monsterHP['health'] > 0:
+        if attackInput =='1':
+         monsterHP['health'] -= playerStats['Attack']
+         playerStats['Health'] -= monsterHP['power']
+         if monsterHP['health'] <= 0:
+           input(f'You won! Gained {monsterHP['money']} Gold\nPress any key to continue.')
+           playerStats['Gold'] += monsterHP['money']
+         elif monsterHP['health'] > 0:
+          attackInput = input(f"Strike agian? 1) Yes (Lose {monsterHP['power']} hp) 2) No\n")
+        elif attackInput =='2':
+            break
+          
 def rest():
+    '''Heals player for 30 health and resets player health from items
+    
+    '''
     print('You feel well rested')
     playerStats['Health'] += 30
     if playerStats['Health'] > 30:
       playerStats['Health'] = 30
-    input('press key to continue')   
+    input('press key to continue') 
+  
 def town_shop():
+    '''My over thought out shop it has weapons and armor and eventually items that the player absorbs to boost stats
+        spend whole day making this just to read i should have made a durability system. please forgive me Jeff! You should
+        teach a class on Godot if you want us to make a game it uses python as well. Game design class would be cool.
+    '''
     print('Welcome to our Amazing Bazzar')
     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
     while shop != '4':
+        #Weapons tab
         if shop == '1':
             for i, d in enumerate(weapoons):
                 print(i+1 ,d['weap'])
             pick = input(f'Choose something will ya!\n')
-            costs = [d['cost'] for d in weapoons]
+            costsw = [d['cost'] for d in weapoons]
             if pick == '1':
-                pick = input(f'Purchase for {costs[0]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[0]:
-                    playerStats['Gold'] -= costs[0]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvW.append(weapoons[0])
+                #This code block is copied everywhere in this func. Lets player buy weapon if they have the coin and choose yes
+                #FIXME for some reason the inn works but this doesnt if the player has too little gold 
+                pick = input(f'Purchase for {costsw[0]}?\ny/n\n')
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > 30:
+                        playerStats['Gold'] -= costsw[0]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvW.append(weapoons[0])
+                    elif playerStats['Gold'] < 30:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
 
             elif pick == '2':
-                pick = input(f'Purchase for {costs[1]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[1]:
-                    playerStats['Gold'] -= costs[1]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvW.append(weapoons[1])
+                pick = input(f'Purchase for {costsw[1]}?\ny/n\n')
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > costsw[1]:
+                        playerStats['Gold'] -= costsw[1]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvW.append(weapoons[1])
+                    elif playerStats['Gold'] < costsw[1]:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
             
             elif pick == '3':
-                pick = input(f'Purchase for {costs[0]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[2]:
-                    playerStats['Gold'] -= costs[2]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvW.append(weapoons[2])
+                pick = input(f'Purchase for {costsw[2]}?\ny/n\n')
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > costsw[2]:
+                        playerStats['Gold'] -= costsw[2]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvW.append(weapoons[2])
+                    elif playerStats['Gold'] < costsw[2]:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
-
+        #Armors Tab
         if shop == '2':
             for i, d in enumerate(armor):
                 print(i+1 ,d['de'])
@@ -142,45 +177,64 @@ def town_shop():
             
             if pick == '1':
                 pick = input(f'Purchase for {costs[0]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[0]:
-                    playerStats['Gold'] -= costs[0]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvA.append(armor[0])
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > costs[0]:
+                        playerStats['Gold'] -= costs[0]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvA.append(armor[0])
+                    elif playerStats['Gold'] < costs[0]:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
             
             elif pick == '2':
                 pick = input(f'Purchase for {costs[1]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[1]:
-                    playerStats['Gold'] -= costs[1]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvA.append(armor[1])
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > costs[1]:
+                        playerStats['Gold'] -= costs[1]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvA.append(armor[1])
+                    elif playerStats['Gold'] < costs[1]:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
 
             elif pick == '3':
                 pick = input(f'Purchase for {costs[2]}?\ny/n\n')
-                if pick == 'y' or 'Y' and playerStats['Gold'] > costs[2]:
-                    playerStats['Gold'] -= costs[2]
-                    input(f'Purchase successful\npress enter to continue')
-                    playerInvA.append(armor[2])
+                if pick == 'y' or 'Y':
+                    if playerStats['Gold'] > costs[2]:
+                        playerStats['Gold'] -= costs[2]
+                        input(f'Purchase successful\npress enter to continue')
+                        playerInvA.append(armor[2])
+                    elif playerStats['Gold'] < costs[2]:
+                        print('Come back with the coin and then we will talk')
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
                 elif pick != 'y' or 'Y':
                     shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')
         if shop == '3':
             print('Not yet')
-            shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n')               
+            shop = input(f'What will it be today, {playerStats["Name"]}\n1) Purchase Weapon\n2) Purchase Armor\n3) Purchase Item\n4) Leave\n') 
+              
 def inv():
+    
+    '''INventory system
+        it is lame and a bit dumb but it does the job
+        1 2 3 is picked as strings through input
+        1 is weapons
+        2 is armor
+        3 is leave
+    '''
     print('What would you like to do?')
     chouce = input(f'1) Equip Weapon\n2) Equip Armor\n3) Leave\n')
     while chouce != '3':
-        print(playerInv)
+        
         
         
         for i, d in enumerate(playerInvW):
-
+            #Two off load inventories for the respective items
+            #No limit to amount of items besides the time it takes to get more than 4 weapons or armors
             weapons = [d['weap'] for d in playerInvW]
         for i, d in enumerate(playerInvA):
             armors = [d['de'] for d in playerInvA]
@@ -196,17 +250,22 @@ def inv():
             
             elif chouce == '2':
                 print('Equiped')
-                playerStats['Attack']+=playerInvW[1]['power']
+                playerStats['Attack'] += playerInvW[1]['power']
+                del playerInvW[1]
+
             
             elif chouce == '3':
                 print('Equiped')
                 playerStats['Attack']+=playerInvW[2]['power']
-            
+                del playerInvW[2]
+
+
             elif chouce == '4':
                 print('Equiped')
                 playerStats['Attack']+=playerInvW[3]['power']
-        
-        if chouce == '2':
+                del playerInvW[3]
+
+        elif chouce == '2':
             print('What would you want to equip?')
             for i, d in enumerate(armors):
              print(i+1, d)
@@ -215,46 +274,68 @@ def inv():
                
             if chouce == '1':
                 print('Equiped')
-                playerStats['Attack']+=playerInvA[0]['ar']
+                playerStats['Health']+=playerInvA[0]['ar']
             
             elif chouce == '2':
                 print('Equiped')
-                playerStats['Attack']+=playerInvA[1]['ar']
-            
+                playerStats['Health']+=playerInvA[1]['ar']
+                del playerInvA[1]
+
             elif chouce == '3':
                 print('Equiped')
-                playerStats['Attack']+=playerInvA[2]['ar']
-            
+                playerStats['Health']+=playerInvA[2]['ar']
+                del playerInvA[2]
             elif chouce == '4':
                 print('Equiped')
-                playerStats['Attack']+=playerInvA[3]['ar']
+                playerStats['Health']+=playerInvA[3]['ar']
+                del playerInvA[3]
             
+        chouce = input(f'1) Equip Weapon\n2) Equip Armor\n3) Leave\n')
+playerStats = {"Name" : "" , "Health":10, "Attack": 1, "Gold": 0}                             
+weapoons = [
+            {'weap':'Sword', 'desc':'Normal Sword', 'cost':30, 'power':3},
+             {'weap':'Spear', 'desc':'A warriors weapon.', 'cost':40, 'power':4}, 
+             {'weap':'Club','desc':'Big Frickin Club', 'cost':10, 'power':1}
+             ]
+def savquit():
+    '''This function saves the game
+        use y or n to choose yes or no to save
+    '''
+    yn = input(f'Would you like to save and quit?\ny/n\n')
+    while yn != 'n' or 'N':
+        global stats_json
+        stats_json = [playerStats, playerInvW, playerInvA]
+        if yn == 'y' or 'Y':
+            with open('save.json', 'w' ) as f:
+                json.dump(stats_json, f, indent=4)
                 
+            input("Save Sucessful")
+            exit()
+        break    
 
-
-
-    chouce = input(f'1) Equip Weapon\n2) Equip Armor\n3) Leave\n')
-            
+def load():
+    ny = input('Load save?\ny/n')
+    global playerStats
+    while ny != 'n' or 'N':
+        if ny == 'y' or 'Y':
+            with open('save.json', 'r' ) as f:
+                loading = f.read()
+                stats = json.loads(loading)
+                playerStats = stats[0]
+                playerInvW = stats[1]
+                playerInvA = stats[2]
                 
-                    
+                
+            input("Load Sucessful")
+            break
 
-
-
-weapoons = [{'weap':'Sword', 'desc':'Normal Sword', 'cost':10, 'power':2}, {'weap':'Spear', 'desc':'A warriors weapon.', 'cost':20, 'power':4}, {'weap':'Hammer','desc':'Big Frickin Hamor', 'cost':15, 'power':3}]
 armor = [
     {'de':'Leather Armor', 'desc':'False sense of Security', 'cost':15, 'ar':5},
     {'de':'Plate Armor', 'desc':'Real sense of Security', 'cost':150, 'ar':50},
     {'de':'Chain Armor', 'desc':'Security', 'cost':75, 'ar':25},
 ]
-playerInv = []
+stats_json = str(playerStats)
 playerInvW= [{'weap':'None', 'power':0}]
-playerInvA = [{'de':'None', 'ar':'None'}]
-
-playerStats = {'Name' : '' , 'Health':30, 'Attack': 2, 'Gold': 30}
-monster = new_random_monster()
-
+playerInvA = [{'de':'None', 'ar': 0}]
 if __name__ == '__main__':
     test_function()
-
-
-
